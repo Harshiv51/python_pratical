c938509587491830a3211eb873d14ddb70bc442d
Harshiv51 <20cs051@charusat.edu.in>
2022-04-14 00:00:01 +0530

    p

--- lab_5.py
+++ lab_5.py
@@ -1,2 +1,10 @@
-str=input()
-print(str.swapcase())
+def pr_1_swap(text):
+   result_str = ""
+   for item in text:
+       if item.isupper():
+           result_str += item.lower()
+       else:
+           result_str += item.upper()
+   return result_str
+print(pr_1_swap("HackerRank.com presents 'Pythonist2'."))
+
